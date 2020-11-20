857d50e2134ec60f8cdc1e1af2f8e1ca577c4242
Kenyon Landt <kjl160030@utdallas.edu>
2020-11-20 15:01:27 -0600

    Pick up and drop objects

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public Transform theDest;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = theDest.position;
+            this.transform.parent = GameObject.Find("Destination").transform;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+}
